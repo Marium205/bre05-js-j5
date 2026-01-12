344d513760c515703e2ecccc38d8ed09a5918b92
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-01-12 11:02:37 +0000

    Exercice 4 : ToDo List﻿

--- exercice-4/index.js
+++ exercice-4/index.js
@@ -1,9 +1,30 @@
-window.addEventListener("DOMContentLoaded", () => {
-    let todos = [
-        "Rappeler l'école",
-        "Faire les courses",
-        "Récupérer le colis à la Poste",
-        "Faire la litière du chat",
-        "Ranger le bureau"
-    ];
+const todos = [
+  "Rappeler l'école",
+  "Faire les courses",
+  "Récupérer le colis à la Poste",
+  "Faire la litière du chat",
+  "Ranger le bureau",
+];
+
+// 1 créee <ul>
+const ul = document.createElement("ul");
+
+// 2 pour chaque tâche, cree <li> + texte + event click
+todos.forEach((task) => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(task));
+
+  // click => barrer /debarrer
+  li.addEventListener("click", () => {
+    if (li.style.textDecoration === "line-through") {
+      li.style.textDecoration = "none";
+    } else {
+      li.style.textDecoration = "line-through";
+    }
+  });
+
+  ul.appendChild(li);
 });
+
+// ajoute la liste dans le body
+document.body.appendChild(ul);
