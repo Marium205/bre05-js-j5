fdb75f153433434130453071abd2b07f59fb6adf
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-01-12 10:43:14 +0000

    Exercice 4 : ToDo List﻿

---
+++ exercice-4/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let todos = [
+        "Rappeler l'école",
+        "Faire les courses",
+        "Récupérer le colis à la Poste",
+        "Faire la litière du chat",
+        "Ranger le bureau"
+    ];
+});
